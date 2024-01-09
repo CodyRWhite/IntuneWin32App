9d98790a31ee22b75512c2489387ccdf5b5a0867
Cody White <codyrwhite@icloud.com>
2024-01-09 00:17:49 -0500

    Update New-IntuneWin32AppPackage.ps1
    test2

--- Public/New-IntuneWin32AppPackage.ps1
+++ Public/New-IntuneWin32AppPackage.ps1
@@ -102,8 +102,10 @@ function New-IntuneWin32AppPackage {
                         if ($ProcessPackage -eq $true) {
                             # Invoke IntuneWinAppUtil.exe with parameter inputs
                             Write-Verbose -Message "Invoking IntuneWinAppUtil.exe to initialize packaging process"
-                            
-                            $PackageInvocation = (Start-Process -FilePath "`"$IntuneWinAppUtilPath`"" -ArgumentList "-c `"$SourceFolder`"", "-s `"$SetupFile`"", "-o `"$OutPutFolder`"", "-q" -PassThru:$true -ErrorAction Stop -NoNewWindow).WaitForExit()
+
+                            [string]$toolargs = "-c ""$($SourceFolder)"" -s ""$($SetupFile)"" -o ""$($OutPutFolder)"" -q"
+                            (Start-Process -FilePath $IntuneWinAppUtilPath -ArgumentList $toolargs -PassThru:$true -ErrorAction Stop -NoNewWindow).WaitForExit()
+                            #$PackageInvocation = (Start-Process -FilePath "`"$IntuneWinAppUtilPath`"" -ArgumentList "-c `"$SourceFolder`"", "-s `"$SetupFile`"", "-o `"$OutPutFolder`"", "-q" -PassThru:$true -ErrorAction Stop -NoNewWindow).WaitForExit()
                             #-Wait  -NoNewWindow -RedirectStandardOutput $false
                             #$PackageInvocation = Invoke-Executable -FilePath $IntuneWinAppUtilPath -Arguments "-c ""$($SourceFolder)"" -s ""$($SetupFile)"" -o ""$($OutPutFolder)"" -q" -RedirectStandardOutput $false
                             if ($PackageInvocation.ExitCode -eq 0) {
